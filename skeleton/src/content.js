47d40574c98db7ee835a2cb96612479e2e9bab67
andyr <forpetessake@protonmail.com>
2018-07-04 16:18:30 +0100

    Update content.js

--- skeleton/src/content.js
+++ skeleton/src/content.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React,{Component} from "react";
 import ReactDOM from "react-dom";
 import style from "./div.css";
 
-class Content extends React.Component {
+class Content extends Component {
 	render() {
 		return <div className={style.background} >Hello</div>
 	}
